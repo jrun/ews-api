3b01ad6032aef18735a904065c0d7d1a43307a4e
jrun <jeremy.burks@gmail.com>
2009-12-14 15:15:42 -0600

    shortcut to id and change_key

--- lib/ews/message.rb
+++ lib/ews/message.rb
@@ -1,6 +1,14 @@
 module EWS
   
   class Message < Model
+    def id
+      @id ||= attrs[:item_id][:id]
+    end
+
+    def change_key
+      @change_key ||= attrs[:item_id][:change_key]
+    end
+    
     def shallow?
       self.body.nil? || self.header.nil?
     end
--- spec/ews/message_spec.rb
+++ spec/ews/message_spec.rb
@@ -1,6 +1,20 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 module EWS
+  
   describe Message do
+    before(:each) do
+      @parser = Parser.new
+      @message = @parser.parse_get_item  response_to_doc(:get_item_all_properties)
+    end
+    
+    it "#id should be the item_id id" do
+      @message.id.should == 'HRlZ'
+    end
+
+    it "#change_key should be the item_id change_key" do
+      @message.change_key.should == '0Tk4V'
+    end
   end
+  
 end
